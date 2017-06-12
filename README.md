0defc4088324db710d38442b36f3596dd981fb10
Patrick D'appollonio <patrick@dappollonio.us>
2017-06-12 11:23:45 -0600

    Update code.
    Signed-off-by: Patrick D'appollonio <dappollonio@hpe.com>

---
+++ README.md
@@ -0,0 +1,14 @@
+# Docker container for a HTTP server
+
+This docker container is just a simple HTTP file server. It will serve a simple file server
+which will show either the contents of the /html folder or the mounted volume contents.
+
+## Container published to the docker registry
+
+
+
+## Use it with Docker standalone
+
+## USe it with Docker Compose
+
+
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+)
+
+var (
+	fileServerPath = "/html"
+	fileServerPort = "5000"
+)
+
+func init() {
+	if v := os.Getenv("FILE_SERVER_PATH"); v != "" {
+		fileServerPath = v
+	}
+
+	if v := os.Getenv("FILE_SERVER_PORT"); v != "" {
+		fileServerPort = v
+	}
+}
+
+func exists(path string) bool {
+	_, err := os.Stat(path)
+
+	if err != nil || os.IsNotExist(err) {
+		return false
+	}
+
+	return true
+}
+
+func main() {
+	// Check if the folder exists
+	if !exists(fileServerPath) {
+		log.Fatalf("Unable to start server because $FILE_SERVER_PATH doesn't exist: %q", fileServerPath)
+	}
+
+	// Create the file server
+	fs := http.FileServer(http.Dir(fileServerPath))
+	http.Handle("/", fs)
+
+	// Graceful shutdown
+	sigquit := make(chan os.Signal, 1)
+	signal.Notify(sigquit, os.Interrupt, os.Kill)
+
+	// Wait signal
+	close := make(chan bool, 1)
+
+	// Create a server
+	srv := &http.Server{Addr: fmt.Sprintf(":%s", fileServerPort)}
+
+	// Execute the server
+	go func() {
+		log.Printf("Starting HTTP Server. Listening at %q", srv.Addr)
+
+		if err := srv.ListenAndServe(); err != nil {
+			if err != http.ErrServerClosed {
+				log.Println(err.Error())
+			} else {
+				log.Println("Server closed. Bye!")
+			}
+			close <- true
+		}
+	}()
+
+	// Check for a closing signal
+	go func() {
+		<-sigquit
+		log.Printf("Gracefully shutting down server...")
+
+		if err := srv.Shutdown(nil); err != nil {
+			log.Println("Unable to shut down server: " + err.Error())
+			close <- true
+		}
+	}()
+
+	<-close
+}
