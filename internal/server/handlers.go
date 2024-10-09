a8c835c26ef517b783e50b6723f32c3f9de0e919
Muse Mulatu <muse@kubefirst.io>
2024-10-08 22:08:29 -0600

    ref: log aciton

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -40,6 +40,7 @@ func (s *Server) showOrRender(w http.ResponseWriter, r *http.Request) {
 		if os.IsNotExist(err) {
 			s.printWarning("attempted to access non-existent path: %s", currentPath)
 			if s.CustomNotFoundPage != "" {
+				fmt.Fprintf(s.LogOutput, "%s %q -- custom %d \n", r.Method, r.RequestURI, http.StatusNotFound)
 				s.serveFile(s.CustomNotFoundPage, w, r)
 				return
 			}
